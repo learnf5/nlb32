4d3e5c111b053ee35ecfb3eea0e74a66d3250c56
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-04-25 08:40:39 -0700

    Add files via upload

---
+++ NLB32-L03L01-Health.sh
@@ -0,0 +1,8 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update lab environment
+sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NLB32/main/lab02/main.conf
+sudo scp /tmp/main.conf nginx:/etc/nginx/conf.d/
