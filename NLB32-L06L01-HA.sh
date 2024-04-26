f3f59f7087df67fc840da70f423fb5a95b6d5b56
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-04-26 10:10:53 -0700

    Update NLB32-L06L01-HA.sh

--- NLB32-L06L01-HA.sh
+++ NLB32-L06L01-HA.sh
@@ -4,7 +4,9 @@ PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NLB32/main/lab06/main.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nlb32/main/lab06/main.conf
 sudo scp /tmp/main.conf nginx:/etc/nginx/conf.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nlb32/main/lab06/tcp_lb.conf
 sudo scp /tmp/tcp_lb.conf nginx:/etc/nginx/conf.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nlb32/main/lab06/nginx.conf
 sudo scp /tmp/nginx.conf nginx:/etc/nginx/
