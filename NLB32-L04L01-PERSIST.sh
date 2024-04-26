71fb347a65c4414d9dad62742771768b3d73dceb
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-04-26 10:11:41 -0700

    Update NLB32-L04L01-PERSIST.sh

--- NLB32-L04L01-PERSIST.sh
+++ NLB32-L04L01-PERSIST.sh
@@ -4,5 +4,5 @@ PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/NLB32/main/lab04/main.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nlb32/main/lab04/main.conf
 sudo scp /tmp/main.conf nginx:/etc/nginx/conf.d/
