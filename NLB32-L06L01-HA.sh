a8b9f05ffbeaf228b42b2da73e3b3c19ef65cfee
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-07-26 12:02:20 -0700

    Update NLB32-L06L01-HA.sh

--- NLB32-L06L01-HA.sh
+++ NLB32-L06L01-HA.sh
@@ -35,6 +35,8 @@ sudo scp /tmp/NGINX_Lab_Guide.desktop nginx2:/home/student/Desktop/Lab_Guide.des
 
 sudo ssh nginx gio set /home/student/Desktop/Lab_Guide.desktop metadata::trusted true
 sudo ssh nginx chmod +x /home/student/Desktop/Lab_Guide.desktop
+sudo ssh nginx chmown student.student /home/student/Desktop/Lab_Guide.desktop
 
 sudo ssh nginx2 gio set /home/student/Desktop/Lab_Guide.desktop metadata::trusted true
 sudo ssh nginx2 chmod +x /home/student/Desktop/Lab_Guide.desktop
+sudo ssh nginx2 chmown student.student /home/student/Desktop/Lab_Guide.desktop
