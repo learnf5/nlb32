34981991c86274d77775d897b4e215fdd3bc741b
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-07-26 12:13:02 -0700

    Update NLB32-L06L01-HA.sh

--- NLB32-L06L01-HA.sh
+++ NLB32-L06L01-HA.sh
@@ -33,10 +33,10 @@ echo Exec=firefox https://f5.bravais.com/s/$brav_id >> /tmp/NGINX_Lab_Guide.desk
 sudo scp /tmp/NGINX_Lab_Guide.desktop nginx:/home/student/Desktop/Lab_Guide.desktop
 sudo scp /tmp/NGINX_Lab_Guide.desktop nginx2:/home/student/Desktop/Lab_Guide.desktop
 
+sudo ssh nginx chown student.student /home/student/Desktop/Lab_Guide.desktop
 sudo ssh nginx gio set /home/student/Desktop/Lab_Guide.desktop metadata::trusted true
 sudo ssh nginx chmod +x /home/student/Desktop/Lab_Guide.desktop
-sudo ssh nginx chown student.student /home/student/Desktop/Lab_Guide.desktop
 
+sudo ssh nginx2 chown student.student /home/student/Desktop/Lab_Guide.desktop
 sudo ssh nginx2 gio set /home/student/Desktop/Lab_Guide.desktop metadata::trusted true
 sudo ssh nginx2 chmod +x /home/student/Desktop/Lab_Guide.desktop
-sudo ssh nginx2 chown student.student /home/student/Desktop/Lab_Guide.desktop
