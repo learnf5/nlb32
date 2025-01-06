38e1c546d55a8821dc9777369f30cd2faab4fef3
djlamey <76072289+djlamey@users.noreply.github.com>
2025-01-06 13:57:18 -0500

    Update common-tasks.sh
    correcting license file names

--- common-tasks.sh
+++ common-tasks.sh
@@ -25,7 +25,8 @@ set +x
 curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/nginx-one-eval.{crt,key,jwt}
 echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/nginx-one-eval.{crt,key,jwt}
 set -x
-until sudo scp /tmp/nginx-one-eval.{crt,key} nginx:/etc/ssl/nginx/ || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
 
 # run this lab's specific tasks saved on GitHub
