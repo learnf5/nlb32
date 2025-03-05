bc8aecbc74950a8ee6bbd0c82cbe98642720afc6
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-03-05 12:07:06 -0800

    Update NLB32-L06L01-HA.sh

--- NLB32-L06L01-HA.sh
+++ NLB32-L06L01-HA.sh
@@ -10,7 +10,8 @@ curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.
 sudo scp /tmp/nginx.conf nginx:/etc/nginx/
 
 #Add nginx license to nginx2 lab system, required for keepalived setup
-sudo scp /tmp/nginx-one-eval.{crt,key} nginx2:/etc/ssl/nginx/
+sudo scp /tmp/nginx-one-eval.crt nginx2:/etc/ssl/nginx/nginx-repo.crt
+sudo scp /tmp/nginx-one-eval.key nginx2:/etc/ssl/nginx/nginx-repo.key
 sudo scp /tmp/nginx-one-eval.jwt nginx2:/etc/nginx/license.jwt
 
 #This tcp directory created in lab 5, so it should carry on
